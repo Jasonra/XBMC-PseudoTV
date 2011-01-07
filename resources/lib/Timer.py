17fb5c1fe3a75cc4369403723d07976a40ecf014
Jason Anderson <jason.ra@gmail.com>
2011-01-06 20:27:55 -0500

    Moved scriptnfiles to the lib directory. Fixed the threading issue on exit.

--- resources/lib/Timer.py
+++ resources/lib/Timer.py
@@ -2,11 +2,11 @@ import xbmc
 import os, threading
 
 
-class Timer(threading.Thread):
+class RealTimer(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.running = False
         self.shouldCancel = False
+        self.running = False
         self.sleepTime = 0.0
 
 
@@ -24,23 +24,27 @@ class Timer(threading.Thread):
         # Don't run the action if the timer was cancelled
         if self.shouldCancel == False:
             self.action()
-
+            
         self.running = False
 
 
+
+class Timer:
+    def __init__(self):
+        self.MyTimer = RealTimer()
+
+
     def stopTimer(self):
-        self.shouldCancel = True
+        self.MyTimer.shouldCancel = True
 
-        if self.running:
-            self.join()
-#         while self.running == 1:
-#             xbmc.sleep(50)
+        if self.MyTimer.running:
+            self.MyTimer.join()
 
-        self.shouldCancel = False
+        self.MyTimer.shouldCancel = False
 
 
     def startTimer(self, sleepms, action):
         self.stopTimer()
-        self.sleepTime = sleepms
-        self.action = action
-        self.start()
+        self.MyTimer.sleepTime = sleepms
+        self.MyTimer.action = action
+#        self.MyTimer.start()
